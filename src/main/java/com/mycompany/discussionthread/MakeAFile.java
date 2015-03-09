ff188750b9dc7b441cf75f59f9d71aeb39508ddc
Brenton <kreiger.brenton@gmail.com git config --global user.email Brenton>
2015-03-09 15:03:34 -0600

    create login file

--- src/main/java/com/mycompany/discussionthread/MakeAFile.java
+++ src/main/java/com/mycompany/discussionthread/MakeAFile.java
@@ -1,12 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package com.mycompany.discussionthread;
 
 import classes.Comment;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -23,7 +25,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "MakeAFile", urlPatterns = {"/MakeAFile"})
 public class MakeAFile extends HttpServlet {
-
+    
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -34,7 +36,7 @@ public class MakeAFile extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException 
+            throws ServletException, IOException
     {
         String dataDirectory = System.getenv("OPENSHIFT_DATA_DIR");
         if (dataDirectory != null)
@@ -45,12 +47,28 @@ public class MakeAFile extends HttpServlet {
         {
             dataDirectory = "userInfo.txt";
         }
-            BufferedWriter writer = new BufferedWriter(new FileWriter(dataDirectory, false));
-            writer.write("Brent,test" + "\n");
-            writer.write("Burton,test" + "\n");
-            writer.close();
+        response.getWriter().write("Trying to write to: " + dataDirectory + '\n');
+        BufferedWriter writer = new BufferedWriter(new FileWriter(dataDirectory, false));
+        writer.write("Brent,test" + "\n");
+        writer.write("Burton,test" + "\n");
+        writer.close();
+        response.getWriter().write("Trying to read\n");
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(dataDirectory));
+            String line;
+            
+            while ((line = reader.readLine()) != null)
+            {
+                response.getWriter().write(line + "\n");
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
-
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -65,7 +83,7 @@ public class MakeAFile extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
+    
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -79,7 +97,7 @@ public class MakeAFile extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
+    
     /**
      * Returns a short description of the servlet.
      *
@@ -89,5 +107,5 @@ public class MakeAFile extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
+    
 }
